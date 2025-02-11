9e7f6bb7e01f5729652282296565f01c72560794
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-11 12:35:35 +0530

    Update sort_0_1_2_approach2.cpp

--- Arrays/sort_0_1_2_approach2.cpp
+++ Arrays/sort_0_1_2_approach2.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void printArray(int arr[],int size){
+  for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+}
+
+//approach 2 -- using sort function of the <algorithm> header
+void sort_0_1_2(int arr[],int size){
+  sort(arr,arr+size);
+}
+  
+int main(){
+  int array[1000];
+  int size;
+  cout<<"Enter the size of the array:";
+  cin>>size;
+  cout<<"Enter the elements of the array(0/1/2):";
+  for(int i=0;i<size;i++){
+    cin>>array[i];
+  }
+  cout<<"Original Array:";
+  printArray(array,size);
+  cout<<"Sorted Array:";
+  sort_0_1_2(array,size);
+  printArray(array,size);
+
+  return 0;
+}
 
