80f1298b09666c281fe311841bc1181553b8e81e
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-11 09:12:11 +0530

    Update array_intersection.cpp

--- Arrays/array_intersection.cpp
+++ Arrays/array_intersection.cpp
@@ -2,9 +2,13 @@
 #include <climits>
 using namespace std;
 
+//brute force approach
 void arrayintersection(int arr1[],int arr2[],int size1,int size2){
   for(int i=0;i<size1;i++){
     for(int j=0;j<size2;j++){
+      if(arr1[i]<arr2[j]){
+        break;
+      }
       if(arr1[i]==arr2[j]){
         cout<<arr1[i]<<" ";
         arr2[j]=INT_MIN;
@@ -31,8 +35,13 @@ int main(){
   for(int i=0;i<size2;i++){
     cin>>arr2[i];
   }
+  cout<<"Using approach 1:"<<endl;
   cout<<"Intersection of both the arrays are:";
   arrayintersection(arr1,arr2,size1,size2);
-
+  cout<<endl;
+  cout<<"Using approach 2:"<<endl;
+  cout<<"Intersection of both the arrays are:";
+  optimised(arr1,arr2,size1,size2);
+  
   return 0;
 }
