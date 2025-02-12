76f13b9741af15085974a3ce690b346b5b56983a
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-12 16:04:32 +0530

    Added function for the problem Remove Element (LeetCode 27)

--- Arrays/remove_element.cpp
+++ Arrays/remove_element.cpp
@@ -1,1 +1,13 @@
+//Function for the problem remove element ( LeetCode 27 )
 
+int removeElement(vector<int>& nums, int val) {
+  int k=0;
+  int index=0;
+  for(int i=0;i<nums.size();i++){
+    if(nums[i]!=val){
+      nums[index++]=nums[i];
+      k++;
+    }
+  }
+  return k;
+}
