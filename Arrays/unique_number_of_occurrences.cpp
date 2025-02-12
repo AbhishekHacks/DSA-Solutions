0616517e8e3210c3918e8bea2fe2cdfc3dff43e0
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-12 13:55:52 +0530

    Added solution to unique_number_of_occurrences.cpp

--- Arrays/unique_number_of_occurrences.cpp
+++ Arrays/unique_number_of_occurrences.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <climits>
+using namespace std;
 
+bool uniqueoccurrences(int arr[],int size){
+  int occurrence[size];
+  int index=0;
+  int count=1;
+  for(int i=0;i<size;i++){
+    if(arr[i]!=INT_MIN){
+      for(int j=i+1;j<size;j++){
+        if(arr[i]==arr[j]){
+          count+=1;
+          arr[j]=INT_MIN;
+        }
+      }
+      for(int i=0;i<index;i++){
+        if(occurrence[i]==count){
+          return 0;
+        }
+      }
+      occurrence[index++]=count;
+      count=1;
+    }
+  }
+  return 1;
+}
+
+int main(){
+  int array[100];
+  cout<<"Enter the size of the array:";
+  int size;
+  cin>>size;
+  cout<<"Enter the elements of the array:";
+  for(int i=0;i<size;i++){
+    cin>>array[i];
+  }
+  cout<<uniqueoccurrences(array,size);
+
+  return 0;
+}
