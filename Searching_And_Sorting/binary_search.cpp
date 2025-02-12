840200c022d287202d0194800df5e9fe7d2eae9e
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-12 16:34:49 +0530

    Added solution in binary_search.cpp

--- Searching_And_Sorting/binary_search.cpp
+++ Searching_And_Sorting/binary_search.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+//Time Complexity -- O(logn)
+int binarySearch(int arr[],int size,int key){
+  int start=0;
+  int end=size-1;
+  while(start<=end){
+    int mid = (start+end)/2;
+    if(arr[mid]==key){
+      return mid;
+    }
+    //when your key is greater than the middle element , just move the start pointer to mid+1
+    else if(arr[mid]<key){
+      start=mid+1;
+    }
+    //when your key is less than the middle element , just move the end pointer to mid-1
+    else{
+      end=mid-1;
+    }
+  }
+
+  return -1;
+}
+
+int main(){
+  int even[6]={2,4,6,8,12,18};
+  int odd[5]={3,8,11,14,16};
+
+  cout<<binarySearch(even,6,9)<<endl;
+  cout<<binarySearch(odd,5,14)<<endl;
+
+  return 0;
+}
