b8616093fd1ac77f3fed91637d222f361a704815
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-13 21:39:07 +0530

    Added solution to Valid Mountain Array problem in valid_mountain_array.cpp

--- Searching_And_Sorting/valid_mountain_array.cpp
+++ Searching_And_Sorting/valid_mountain_array.cpp
@@ -1,1 +1,32 @@
+/*Given an array of integers arr, return true if and only if it is a valid mountain array.*/
 
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int step=0;
+        int index=-1;
+        for(int i=0;i<arr.size()-1;i++){
+          if(step==0){
+            if(i==0&&arr[i]>=arr[i+1]){
+              return 0;
+            }
+            if(arr[i]==arr[i+1]){
+              return 0;
+            }
+            if(arr[i]>arr[i+1]){
+              index=i;
+              step=1;
+            }
+          }
+          else{
+            if(arr[i]<=arr[i+1]){
+              return 0;
+            }
+          }
+        }
+      if(index==-1){
+        return 0;
+      }
+      return 1;
+    }
+};
