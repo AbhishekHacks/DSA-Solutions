57b871a0b34be9784a063934e799f37cf9c9e3eb
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-14 22:14:01 +0530

    Added better and simple approach for Rotate Array problem (LeetCode - 189)

--- Arrays/rotate_array_solution2.cpp
+++ Arrays/rotate_array_solution2.cpp
@@ -1,1 +1,21 @@
+/*Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.*/
+// A solid approach to solve this question is provided below --> passes every test cases
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        swap(nums,0,nums.size()-k-1);
+        swap(nums,nums.size()-k,nums.size()-1);
+        swap(nums,0,nums.size()-1);
+    }
+    void swap(vector<int>& nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
+};
