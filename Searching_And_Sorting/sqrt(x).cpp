f3e53e1db963a078285eaca69aefbcff98cdcf34
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-15 11:52:05 +0530

    Added solution of Sqrt(x) problem (LeetCode - 69) in sqrt(x).cpp

--- Searching_And_Sorting/sqrt(x).cpp
+++ Searching_And_Sorting/sqrt(x).cpp
@@ -1,1 +1,26 @@
+/*Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 
+You must not use any built-in exponent function or operator.*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start=0;
+        int end=x;
+        int ans=0;
+        while(start<=end){
+            long long mid = start+(end-start)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid>x){
+                end=mid-1;
+            }
+            else{
+                ans=mid;
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+};
