676f59b815fbe20a6a9093fc94b419dc00bad050
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-16 13:15:53 +0530

    Added solution for Factorial Trailing Zeroes problem (LeetCode - 172) in factorial_trailing_zeroes.cpp

--- Math/factorial_trailing_zeroes.cpp
+++ Math/factorial_trailing_zeroes.cpp
@@ -1,1 +1,12 @@
+/*Given an integer n, return the number of trailing zeroes in n!.
 
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.*/
+
+// If the constraint is given as 0 <= n <= 10^4 than this is the best solution with O(1) time
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        return n/3125+n/625+n/125+n/25+n/5;
+    }
+};
