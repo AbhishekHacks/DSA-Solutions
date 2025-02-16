17ddebda755ae3ee904721a2a22e3588acb86cc1
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-16 18:35:44 +0530

    Added solution for Selection Sort problem in selection_sort.cpp

--- Searching_And_Sorting/selection_sort.cpp
+++ Searching_And_Sorting/selection_sort.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+void selectionsort(int arr[],int size){
+  for(int i=0;i<size-1;i++){
+    int minimum=arr[i];
+    int index=i;
+    for(int j=i;j<size;j++){
+      if(minimum>arr[j]){
+        minimum=arr[j];
+        index=j;
+      }
+    }
+    //swap
+    int temp = arr[i];
+    arr[i]=minimum;
+    arr[index]=temp;
+  }
+}
+int main(){
+  int arr[100];
+  int size;
+  cout<<"Enter the size of the array:";
+  cin>>size;
+  cout<<"Enter the elements of the array:";
+  for(int i=0;i<size;i++){
+    cin>>arr[i];
+  }
+  cout<<"Original Array:";
+  for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  cout<<"Array after Selection Sort:";
+  selectionsort(arr,size);
+  for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+  }
+}
