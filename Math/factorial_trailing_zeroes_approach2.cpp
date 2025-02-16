4efef5b6819be08cc17a4c7757d618a69b86303a
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-16 13:24:42 +0530

    Added O(n*log(n)) solution for Factorial Trailing Zeroes problem (LeetCode - 172 ) in factorial_trailing_zeroes_approach2.cpp

--- Math/factorial_trailing_zeroes_approach2.cpp
+++ Math/factorial_trailing_zeroes_approach2.cpp
@@ -1,1 +1,20 @@
+/*Given an integer n, return the number of trailing zeroes in n!.
 
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.*/
+
+//Another solution with O(n*log5(n)) time complexity
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        for(int i=0;i<=n;i+=5){
+            int element=i;
+            while(element>0&&element%5==0){
+                count++;
+                element/=5;
+            }
+        }
+        return count;
+    }
+};
