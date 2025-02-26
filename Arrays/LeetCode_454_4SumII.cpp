c670ca66961ee008bf6abb143206d88f2d3462a1
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-02-26 19:10:48 +0530

    Added brute force approach for problem 454 (4Sum II) in LeetCode_454_4SumII.cpp

--- Arrays/LeetCode_454_4SumII.cpp
+++ Arrays/LeetCode_454_4SumII.cpp
@@ -3,3 +3,24 @@
 0 <= i, j, k, l < n
 nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0*/
 
+//Brute Force approach -- O(n4)
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        //Brute Force approach -- O(n4)
+        int count=0;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                for(int k=0;k<nums3.size();k++){
+                    for(int l=0;l<nums4.size();l++){
+                        if(nums1[i]+nums2[j]+nums3[k]+nums4[l]==0){
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
