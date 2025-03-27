0c233701369a22a1ff45fe5d370aac5475b438c1
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-03-27 16:32:40 +0530

    Added algorithm for LeetCode problem 452 in LeetCode_452.cpp

--- Arrays/LeetCode_452.cpp
+++ Arrays/LeetCode_452.cpp
@@ -4,4 +4,48 @@ Arrows can be shot up directly vertically (in the positive y-direction) from dif
 
 Given the array points, return the minimum number of arrows that must be shot to burst all balloons.*/
 
-//Time Complexity -- O()
+//Time Complexity -- O(NlogN)
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        
+        int n=points.size();
+
+        //sort the input 2D vector points
+        sort(points.begin(),points.end(),[](const vector<int>&a , const vector<int>&b){
+            return a[1]<b[1];
+        });
+
+        //take two pointers to check if two intervals overlap
+        int opening1=points[0][0];
+        int closing1=points[0][1];
+
+        //one variable 'count' to count the number of arrows to burst all the balloons
+        int count=0;
+
+        //iterate the array to check the intervals(overlapping)
+        for(int i=1;i<n;i++){
+            int opening2=points[i][0];
+            int closing2=points[i][1];
+
+            //check if one ,two or more balloons can be burst by an arrow
+            if(opening2<=closing1){
+                continue;
+            }
+
+            //if not , increase count
+            else{
+                count++; //one arrow shot
+                opening1=opening2;
+                closing1=closing2;
+            }
+        }
+
+        //afterwards we see that there is still either one balloon left or overlapping balloons left , which can be shot by one arrow so we add one more arrow to our answer
+        count+=1;
+
+        //return the number of arrows shot
+        return count;
+    }
+};
