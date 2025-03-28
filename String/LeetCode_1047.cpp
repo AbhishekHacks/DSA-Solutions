0358190b0960ac81d500516f39d6ae465d6531f5
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-03-29 01:47:38 +0530

    Added algorithm for LeetCode problem 1047 in LeetCode_1047.cpp

--- String/LeetCode_1047.cpp
+++ String/LeetCode_1047.cpp
@@ -1,1 +1,28 @@
+/*You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+*/
+
+//Time Complexity -- O(N)
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+        string st;
+
+        for(char ch:s){
+            if(!st.empty()&&st.back()==ch){
+                st.pop_back();
+            }
+            else{
+                st.push_back(ch);
+            }
+        }
+
+        return st;
+    }
+};
