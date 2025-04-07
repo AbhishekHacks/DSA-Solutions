8212d043a76669c4c4168a0a15d44090c1e20538
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-04-08 00:27:46 +0530

    Added algorithm for LeetCode problem 1318 in LeetCode_1318.cpp

--- Bit_Manipulation/LeetCode_1318.cpp
+++ Bit_Manipulation/LeetCode_1318.cpp
@@ -3,5 +3,37 @@ Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1
 
 */
 
-//Time Complexity -- O()
+//Time Complexity -- O(1)
 
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        
+        int flip=0;
+
+        while(a>0 || b>0 || c>0){
+            int r = (c&1);
+            int d1 = (a&1);
+            int d2 = (b&1);
+            if(r==0){
+                if(d1!=0 && d2!=0){
+                    flip+=2;
+                }
+                else if(d1!=0 || d2!=0){
+                    flip+=1;
+                }
+            }
+            else{
+                if(d1==0&&d2==0){
+                    flip+=1;
+                }
+            }
+
+            a=a>>1;
+            b=b>>1;
+            c=c>>1;
+        }
+
+        return flip;
+    }
+};
