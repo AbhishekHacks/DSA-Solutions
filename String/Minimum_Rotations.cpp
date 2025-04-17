ddadbbd83fbca369e1da6ed7e27a9878eb6ce609
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-04-17 12:45:25 +0530

    Added algorithm for Minimum Rotations problem (CodingNinjas) in Minimum_Rotations.cpp

---
+++ String/Minimum_Rotations.cpp
@@ -0,0 +1,26 @@
+/*You’re given a string 'S' of length 'N'. Your task is to find the minimum number of rotations to get the same string 'S'.
+
+NOTE :
+The string only consists of lowercase English alphabets.*/
+
+//Time Complexity -- O()
+
+#include <bits/stdc++.h> 
+int minimumRotations(int n, string s)
+{
+	// Write your code here
+	char ch = s.front();
+	int count=0;
+	for(int i=n-1;i>=0;i--){
+		count++;
+		if(s[i]==ch){
+			string s1 = s.substr(i,n-i-2);
+			string s2 = s.substr(0,i);
+			string temp = s1.append(s2);
+			if(temp==s){
+				break;
+			}
+		}
+	}
+	return count;
+}
