bb4defe6ccb19bdf64a62ae497384a49fe4ac3da
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-04-29 18:07:43 +0530

    Added algorithm for LeetCode problem 2962 in LeetCode_2962.cpp

--- Arrays/LeetCode_2962.cpp
+++ Arrays/LeetCode_2962.cpp
@@ -8,3 +8,46 @@ A subarray is a contiguous sequence of elements within an array.
 
 //Time Complexity -- O(n)
 
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        
+        int n =nums.size();
+
+        int maximum = INT_MIN;
+        for(int i=0;i<n;i++){
+            maximum=max(maximum,nums[i]);
+        }
+
+        long long answer=0;
+
+        vector<int>index_of_maximum;
+        for(int i=0;i<n;i++){
+            if(nums[i]==maximum){
+                index_of_maximum.push_back(i);
+            }
+        }
+
+        if(k>index_of_maximum.size()){
+            return 0;
+        }
+
+        int startIndex=0;
+        for(int i=0;i<n;i++){
+            if(startIndex+k-1>=index_of_maximum.size()){
+                break;
+            }
+            if(i==index_of_maximum[startIndex]){
+                int next_index = index_of_maximum[startIndex+k-1];
+                answer+=(n-next_index);
+                startIndex++;
+            }
+            else{
+                int next_index = index_of_maximum[startIndex+k-1];
+                answer+=(n-next_index);
+            }
+        }
+
+        return answer;
+    }
+};
