fc3f61086dd5db2c63c935c57d51c9f79bc4ea8c
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-05-27 00:36:00 +0530

    Added algorithm for LeetCode problem 560 in LeetCode_560.cpp

---
+++ LeetCode_560.cpp
@@ -0,0 +1,40 @@
+/*Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+*/
+
+//Time Complexity -- O(n)
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        //optimise  -- O(n)
+        //prefix sum
+        int n = nums.size();
+        vector<int>prefix;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            prefix.push_back(sum);
+        }
+        unordered_map<int,int>m;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(prefix[i]==k){
+                count+=1;
+                if(m.count(0)){
+                    count+=m[0];
+                }
+            }
+            else{
+                int diff = prefix[i]-k;
+                if(m.count(diff)){
+                    count+=m[diff];
+                }
+            }
+            m[prefix[i]]++;
+        }
+        return count;
+    }
+};
