b921651a5d62bcef95ae191a0402dde8cdc3fcb2
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-07-02 13:08:24 +0530

    Added algorithm for LeetCode problem 58 in LeetCode_58.cpp

--- String/LeetCode_58.cpp
+++ String/LeetCode_58.cpp
@@ -1,33 +1,23 @@
-/*Given a string s consisting of words and spaces, return the length of the last word in the string.
-
-A word is a maximal substring consisting of non-space characters only.*/
-
-//Time Complexity -- O(N)
-
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
         int n = s.size();
-
-        //logic --> start iterating from the end and when you encounter spaces just igmore them but as soon as you encounter a character increase the count variable and when at any point you encounter a space just break the loop and return the length(count)
-        bool space = false;
-        bool character = false;
         int count=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==' '&&character==false){
-                continue;
-            }
-            else if(s[i]!=' '){
+        int len = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
                 count++;
-                character=true;
             }
-            else if(s[i]==' '&&character==true){
-                break;
+            else{
+                if(count>0){
+                    len = count;
+                }
+                count=0;
             }
         }
-
-        //return the length
-        return count;
+        if(count!=0){
+            len=count;
+        }
+        return len;
     }
 };
